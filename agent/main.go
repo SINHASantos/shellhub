8065eb77c175a8ce4f25902053ccdfc67089e291
Eduardo Kluwe Veiga <eduardoveiga@protonmail.ch>
2020-08-10 18:47:32 -0300

    wip

--- agent/main.go
+++ agent/main.go
@@ -171,11 +171,11 @@ func main() {
 			}
 		}()
 	}
-	fmt.Printf("\n%+v\n", opts)
 
 	ticker := time.NewTicker(10 * time.Second)
 
 	for range ticker.C {
+		fmt.Printf("\n%+v\n", opts)
 		sessions := make([]string, 0, len(server.Sessions))
 		for key := range server.Sessions {
 			sessions = append(sessions, key)
